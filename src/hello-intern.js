1cb8fd8ea4c82ea7d35efd7ede2ca39835bcfd23
coddicod-van <vanparmar44@gmail.com>
2025-12-02 14:41:26 +0530

    Initial commit with template content

---
+++ src/hello-intern.js
@@ -0,0 +1,9 @@
+// src/hello-intern.js
+// This is a simple script to confirm your Node.js environment is set up correctly.
+
+console.log("-----------------------------------------");
+console.log("🚀 Hello Intern! Welcome to StaytunedLABS!");
+console.log("-----------------------------------------");
+console.log("Your environment is ready. You can now start coding!");
+
+
